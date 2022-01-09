2be9e39e92952091937a06f552b367027e0b7b28
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-01-09 08:44:35 -0600

    add for loop code

--- forloops/go-forloops.go
+++ forloops/go-forloops.go
@@ -1,6 +1,13 @@
 package forloops
 
+import "fmt"
+
 // Basic for loop syntax
 func BasicForLoop() {
+	sum := 0
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
 
+	fmt.Println("sum of 1..100 is", sum)
 }
