e3137fee8e327d897c00005ce791337966525b36
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-03-01 16:31:18 -0600

    adding online store challenge instructions

---
+++ challenges/online-store.go
@@ -0,0 +1,18 @@
+package challenges
+
+/*
+
+TODO
+
+Create a package to manage an online store
+Write a program that uses a custom package to manage accounts for an online store. Your challenge includes the following four elements:
+
+Create a custom type called Account that includes the first and last name of the account owner. The type must also include the functionality to ChangeName.
+
+Create another custom type called Employee that includes a variable to store the number of credits as type float64 and that embeds the Account object. The type must also include the functionality to AddCredits, RemoveCredits, and CheckCredits. You need to demonstrate that you can change the account name via the Employee object.
+
+Write a Stringer method to your Account object so that the Employee name can be printed out in a format that includes the first and last name.
+
+Finally, write a program that consumes the package you created, and test all the functionality listed in this challenge. That is, the main program should change the name, print the name, add credit, remove credit, and check the balance.
+
+*/
