0da344d6b6cb890e6bbb55201239d8169b5dc253
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-03-01 14:51:52 -0600

    learning more about methods

--- methods/methods.go
+++ methods/methods.go
@@ -4,13 +4,28 @@ import "fmt"
 
 func DeclareMethod() {
 	t := triangle{3}
-	fmt.Println("Perimeter:", t.perimeter())
+	s := square{4}
+	fmt.Println("\r\nPerimeter (triangle):", t.perimeter())
+	fmt.Println("\r\nPerimeter (square):", s.perimeter())
 }
 
+// Structs
 type triangle struct {
 	size int
 }
 
+type square struct {
+	size int
+}
+
+// Methods
+
+// This method requires a receiver, the triangle struct.
 func (t triangle) perimeter() int {
 	return t.size * 3
+}
+
+// Go allows methods with the same name as long as the receivers are different.
+func (s square) perimeter() int {
+	return s.size * 4
 }
